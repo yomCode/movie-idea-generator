d90c956c429821c190cb4c0b7ed9300901bbbe92
Abayomi Mustapha <mustaphaabayomi71@gmail.com>
2023-06-29 20:56:56 +0100

    Image fetched but still needs some organizing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -18,6 +18,9 @@ const Main = () => {
   const [response, setResponse] = React.useState<string>("");
   const [synopsis, setSynopsis] = React.useState<string>("");
   const [title, setTitle] = React.useState<string>("");
+  const [cast, setCast] = React.useState<string>("");
+  const [imagePrompt, setImagePrompt] = React.useState<string>("");
+  const [imageUrl, setImageUrl] = React.useState<string>("");
 
   const apiKey = Process?.env?.REACT_APP_API_KEY;
   //   const url = Process?.env?.REACT_APP_URL;
@@ -59,7 +62,9 @@ const Main = () => {
     try {
       const response: any = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+        prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. 
+                The synopsis should include actors names in brackets after each character. 
+                Choose actors that would be ideal for this role.
                 ###
                 outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
                 synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. 
@@ -103,17 +108,79 @@ const Main = () => {
     }
   };
 
+  const fetchCast = async (synopsis: string) => {
+    try {
+      const response: any = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Extract the names in brackets from the synopsis.
+                ###
+                synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+                names: Tom Cruise, Val Kilmer, Kelly McGillis
+                ###
+                synopsis: ${synopsis}
+                names:   
+                `,
+        max_tokens: 30,
+      });
+      setCast(response?.data?.choices[0]?.text?.trim());
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const generateImagePrompt = async (title: string, synopsis: string) => {
+    try {
+      const response: any = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Give a short description of an image which could be used to advertise a movie based on a title and synopsis. The description should be rich in visual detail but contain no names.
+                ###
+                title: Love's Time Warp
+                synopsis: When scientist and time traveller Wendy (Emma Watson) is sent back to the 1920s to assassinate a future dictator, she never expected to fall in love with them. As Wendy infiltrates the dictator's inner circle, she soon finds herself torn between her mission and her growing feelings for the leader (Brie Larson). With the help of a mysterious stranger from the future (Josh Brolin), Wendy must decide whether to carry out her mission or follow her heart. But the choices she makes in the 1920s will have far-reaching consequences that reverberate through the ages.
+                image description: A silhouetted figure stands in the shadows of a 1920s speakeasy, her face turned away from the camera. In the background, two people are dancing in the dim light, one wearing a flapper-style dress and the other wearing a dapper suit. A semi-transparent image of war is super-imposed over the scene.
+                ###
+                title: zero Earth
+                synopsis: When bodyguard Kob (Daniel Radcliffe) is recruited by the United Nations to save planet Earth from the sinister Simm (John Malkovich), an alien lord with a plan to take over the world, he reluctantly accepts the challenge. With the help of his loyal sidekick, a brave and resourceful hamster named Gizmo (Gaten Matarazzo), Kob embarks on a perilous mission to destroy Simm. Along the way, he discovers a newfound courage and strength as he battles Simm's merciless forces. With the fate of the world in his hands, Kob must find a way to defeat the alien lord and save the planet.
+                image description: A tired and bloodied bodyguard and hamster standing atop a tall skyscraper, looking out over a vibrant cityscape, with a rainbow in the sky above them.
+                ###
+                title: ${title}
+                synopsis: ${synopsis}
+                image description:
+                `,
+        max_tokens: 100,
+        temperature: 0.8,
+      });
+      setImagePrompt(response?.data?.choices[0]?.text?.trim());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchImageUrl = async (imagePrompt: string) => {
+    try {
+      const response: any = await openai.createImage({
+        prompt: `${imagePrompt}. There should be no text in this image.`,
+        n: 1,
+        size: "256x256",
+        response_format: "b64_json",
+      });
+      setImageUrl(response.data.data[0].b64_json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleSend = (e: React.FormEvent<IdeaDescription>) => {
     e.preventDefault();
     const text = e.currentTarget.elements.text?.value;
     fetchTextCompletion(text);
     fetchSynopsis(text);
     fetchTitle(synopsis);
+    fetchCast(synopsis);
+    generateImagePrompt(title, synopsis);
+    fetchImageUrl(imagePrompt);
     e.currentTarget.elements.text.value = "";
   };
-
-  console.log({ loading });
-  console.log({ response, synopsis });
 
   return (
     <main>
@@ -170,9 +237,11 @@ const Main = () => {
       {/* <!-- The Output -->	 */}
       {synopsis?.length && (
         <section className="output-container" id="output-container">
-          <div id="output-img-container" className="output-img-container"></div>
+          <div id="output-img-container" className="output-img-container">
+            <img src={`data:image/png;base64,${imageUrl}`} alt="" />
+          </div>
           <h1 id="output-title">{title}</h1>
-          <h2 id="output-stars">{}</h2>
+          <h2 id="output-stars">{cast}</h2>
           <p id="output-text">{synopsis}</p>
         </section>
       )}
